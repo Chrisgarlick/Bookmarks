b2eaf2179e39d13f656acb1373d8e21c0ac17e17
Chrisgarlick <cgarlick94@gmail.com>
2021-02-26 11:31:38 +0000

    Up to page 153 or 128

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -6,4 +6,8 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name="register"),
+    path('edit/', views.edit, name="edit"),
 ]
